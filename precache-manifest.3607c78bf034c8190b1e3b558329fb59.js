7658e7ad0fb53ca16e868e586e218751b89494da
Hector Ricardom <hectoricardom@gmail.com>
2019-08-11 00:57:07 -0400

    bn45y64gwv

---
+++ precache-manifest.3607c78bf034c8190b1e3b558329fb59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2b8c702c44594132b9c",
+    "url": "/static/css/main.4501bc58.chunk.css"
+  },
+  {
+    "revision": "e2b8c702c44594132b9c",
+    "url": "/static/js/main.e2b8c702.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "b2667b3d75544dad1e22",
+    "url": "/static/js/2.b2667b3d.chunk.js"
+  },
+  {
+    "revision": "9784c289f453be9f0f65a18b7ef3446a",
+    "url": "/index.html"
+  }
+];
