406f03658d0b9623570f9385dc23b92da06eeeee
Hector Ricardom <hectoricardom@gmail.com>
2019-08-26 10:23:39 -0400

    3v6uy35

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3348c9fcb9de5f622aa2155a039d5d08.js"
+  "/precache-manifest.db10400126e5422742ce1f3d570b5589.js"
 );
 
 workbox.clientsClaim();
