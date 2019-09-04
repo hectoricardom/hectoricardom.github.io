2e7f350a62164d5d105706a106dfce13922ba7c6
Hector Ricardom <hectoricardom@gmail.com>
2019-09-04 18:30:22 -0400

    dy52f4g6w

---
+++ precache-manifest.1e35f18dd03b0958210cec5b57c31e42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfe21d35dd47a25971b",
+    "url": "/static/css/main.2670ed47.chunk.css"
+  },
+  {
+    "revision": "fdfe21d35dd47a25971b",
+    "url": "/static/js/main.fdfe21d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "d08e6de15d871b3c6726",
+    "url": "/static/js/2.d08e6de1.chunk.js"
+  },
+  {
+    "revision": "1ce778bc4a0efed99f93e519b86967d7",
+    "url": "/index.html"
+  }
+];
