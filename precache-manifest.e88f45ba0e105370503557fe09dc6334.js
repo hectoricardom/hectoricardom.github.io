eccac4a7626ebdac5a6a6a109064071507cfa1af
Hector Ricardom <hectoricardom@gmail.com>
2020-03-06 15:41:23 -0500

    sdkjfapbdgsag

---
+++ precache-manifest.e88f45ba0e105370503557fe09dc6334.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "171cf4cbcb08f12f8359",
+    "url": "/static/css/main.bf25fde7.chunk.css"
+  },
+  {
+    "revision": "171cf4cbcb08f12f8359",
+    "url": "/static/js/main.4542d2f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eeb37f763b24792ad7dc",
+    "url": "/static/js/2.64eb7fa8.chunk.js"
+  },
+  {
+    "revision": "d4fdd8a5888d4bebe3e76ac7d7c15753",
+    "url": "/index.html"
+  }
+];
